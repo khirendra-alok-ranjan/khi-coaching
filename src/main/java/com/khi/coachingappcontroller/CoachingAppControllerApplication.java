7896cd0bd0fe3104966cd84d605213371ffbf94a
KHIRENDRA ALOK RANJAN <khirendra.alokranjan3@gmail.com>
2024-04-07 21:32:38 +0530

    added coaching project to local repo

---
+++ src/main/java/com/khi/coachingappcontroller/CoachingAppControllerApplication.java
@@ -0,0 +1,13 @@
+package com.khi.coachingappcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingAppControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingAppControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khi/coachingappcontroller/CoachingAppControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khi.coachingappcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingAppControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
