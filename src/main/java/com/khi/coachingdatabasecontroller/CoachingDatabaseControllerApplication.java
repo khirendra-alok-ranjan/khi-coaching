4dc3322ef10f40c00ae2fa1606804b314950e419
KHIRENDRA ALOK RANJAN <khirendra.alokranjan3@gmail.com>
2024-04-07 21:48:21 +0530

    first commit

---
+++ src/main/java/com/khi/coachingdatabasecontroller/CoachingDatabaseControllerApplication.java
@@ -0,0 +1,13 @@
+package com.khi.coachingdatabasecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingDatabaseControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingDatabaseControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khi/coachingdatabasecontroller/CoachingDatabaseControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khi.coachingdatabasecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingDatabaseControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
